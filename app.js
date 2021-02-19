bb877e7418251881182101589a301b37afa925ce
Herllon Cardoso <herlloncardoso@pop-os.localdomain>
2021-02-19 09:46:42 -0300

    avgFinalists and avgByChange aggregation pipeline done

--- app.js
+++ app.js
@@ -1,83 +1,85 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
 
-const circulationRepo = require('./repos/circulationRepo')
-const data = require('./circulation.json')
+const circulationRepo = require("./repos/circulationRepo");
+const data = require("./circulation.json");
 
+const url = "mongodb://localhost:27017";
+const dbName = "circulation";
 
-const url = 'mongodb://localhost:27017';
-const dbName =  'circulation';
-
-async function main () {
+async function main() {
   const client = new MongoClient(url);
 
   await client.connect();
 
   try {
+    const results = await circulationRepo.loadData(data);
+    assert.strictEqual(data.length, results.insertedCount);
 
-  const results = await circulationRepo.loadData(data)
-  assert.strictEqual(data.length, results.insertedCount)
-  
-  const getData = await circulationRepo.get()
-  assert.strictEqual(data.length, getData.length)
+    const getData = await circulationRepo.get();
+    assert.strictEqual(data.length, getData.length);
 
-  const filterData = await circulationRepo.get({Newspaper: getData[4].Newspaper})
-  assert.deepStrictEqual(filterData[0], getData[4]);
+    const filterData = await circulationRepo.get({
+      Newspaper: getData[4].Newspaper,
+    });
+    assert.deepStrictEqual(filterData[0], getData[4]);
 
-  const limitData = await circulationRepo.get({}, 3);
-  assert.strictEqual(limitData.length, 3)
-  
-  const id = getData[4]._id.toString()
-  const byId = await circulationRepo.getById(getData[4]._id)
-  assert.deepStrictEqual(byId, getData[4])
+    const limitData = await circulationRepo.get({}, 3);
+    assert.strictEqual(limitData.length, 3);
 
-  const newItem = { 
-      "Newspaper": "Boston Herald",
+    const id = getData[4]._id;
+    const byId = await circulationRepo.getById(id);
+    assert.deepStrictEqual(byId, getData[4]);
+
+    const newItem = {
+      Newspaper: "Boston Herald",
       "Daily Circulation, 2004": 1,
       "Daily Circulation, 2013": 2,
       "Change in Daily Circulation, 2004-2013": 100,
       "Pulitzer Prize Winners and Finalists, 1990-2003": 0,
       "Pulitzer Prize Winners and Finalists, 2004-2014": 0,
-      "Pulitzer Prize Winners and Finalists, 1990-2014": 0
-  }
+      "Pulitzer Prize Winners and Finalists, 1990-2014": 0,
+    };
 
-  const addedItem = await circulationRepo.add(newItem)
-  assert(addedItem._id)
-  const addedItemQuery = await circulationRepo.getById(addedItem._id)
-  assert.deepStrictEqual(addedItemQuery, newItem)
+    const addedItem = await circulationRepo.add(newItem);
+    assert(addedItem._id);
+    const addedItemQuery = await circulationRepo.getById(addedItem._id);
+    assert.deepStrictEqual(addedItemQuery, newItem);
 
-  const updatedItem = await circulationRepo.update(addedItem._id, { 
-      "Newspaper": "My new paper",
+    const updatedItem = await circulationRepo.update(addedItem._id, {
+      Newspaper: "My new paper",
       "Daily Circulation, 2004": 1,
       "Daily Circulation, 2013": 2,
       "Change in Daily Circulation, 2004-2013": 100,
       "Pulitzer Prize Winners and Finalists, 1990-2003": 0,
       "Pulitzer Prize Winners and Finalists, 2004-2014": 0,
-      "Pulitzer Prize Winners and Finalists, 1990-2014": 0
-  })
-  assert.strictEqual(updatedItem.Newspaper, "My new paper")
+      "Pulitzer Prize Winners and Finalists, 1990-2014": 0,
+    });
+    assert.strictEqual(updatedItem.Newspaper, "My new paper");
 
-  const newAddedItemQuery = await circulationRepo.getById(addedItem._id)
-  assert.strictEqual(newAddedItemQuery.Newspaper, 'My new paper')
+    const newAddedItemQuery = await circulationRepo.getById(addedItem._id);
+    assert.strictEqual(newAddedItemQuery.Newspaper, "My new paper");
 
-  const removed = await circulationRepo.remove(addedItem._id);
-  assert(removed)
-  const deletedItem = await circulationRepo.getById(addedItem._id);
-  assert.strictEqual(deletedItem, null)
-    
+    const removed = await circulationRepo.remove(addedItem._id);
+    assert(removed);
+    const deletedItem = await circulationRepo.getById(addedItem._id);
+    assert.strictEqual(deletedItem, null);
+
+    const avgFinalists = await circulationRepo.averageFinalists();
+    console.log("Average Finalists:", avgFinalists);
+
+    const avgByChange = await circulationRepo.averageFinalistsByChange();
+    console.log("Average By Change:", avgByChange);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   } finally {
+    const admin = client.db(dbName).admin();
 
-  const admin = client.db(dbName).admin();
+    await client.db(dbName).dropDatabase();
+    console.log(await admin.listDatabases());
 
-  await client.db(dbName).dropDatabase();
-  console.log(await admin.listDatabases());
-
-  client.close();
+    client.close();
   }
-  
 }
 
-
-main()
+main();
--- repos/circulationRepo.js
+++ repos/circulationRepo.js
@@ -1,11 +1,8 @@
-const { MongoClient, ObjectID } = require('mongodb');
-
-
+const { MongoClient, ObjectID } = require("mongodb");
 
 function circulationRepo() {
-
-  const url = 'mongodb://localhost:27017';
-  const dbName =  'circulation';
+  const url = "mongodb://localhost:27017";
+  const dbName = "circulation";
 
   function getById(id) {
     return new Promise(async (resolve, reject) => {
@@ -13,104 +10,182 @@ function circulationRepo() {
       try {
         await client.connect();
         const db = client.db(dbName);
-        const item = await db.collection('newspapers').findOne({_id: ObjectID(id)})
-
+        const item = await db
+          .collection("newspapers")
+          .findOne({ _id: ObjectID(id) });
         resolve(item);
       } catch (error) {
-        reject(error)
+        reject(error);
       }
-    })
+    });
   }
- 
+
   function get(query, limit) {
     return new Promise(async (resolve, reject) => {
       const client = new MongoClient(url, dbName);
       try {
-        await client.connect()
+        await client.connect();
         const db = client.db(dbName);
 
-        let items = db.collection('newspapers').find(query);
+        let items = db.collection("newspapers").find(query);
 
-        if(limit > 0) {
-          items = items.limit(limit)
+        if (limit > 0) {
+          items = items.limit(limit);
         }
         resolve(await items.toArray());
-        client.close()
+        client.close();
       } catch (error) {
-        reject(error)
+        reject(error);
       }
-    })
-
-    
+    });
   }
 
   function loadData(data) {
     return new Promise(async (resolve, reject) => {
       const client = new MongoClient(url);
-      try{
+      try {
         await client.connect();
         const db = client.db(dbName);
-
-        results = await db.collection('newspapers').insertMany(data);
+        const results = await db.collection("newspapers").insertMany(data);
         resolve(results);
         client.close();
-
-      } catch(error) {
-        reject(error)
+      } catch (error) {
+        reject(error);
       }
-    })
-
+    });
   }
 
-  function add(item){
-    return new Promise(async(resolve, reject) => {
+  function add(item) {
+    return new Promise(async (resolve, reject) => {
       const client = new MongoClient(url);
       try {
         await client.connect();
         const db = client.db(dbName);
-        const addedItem = await db.collection('newspapers').insertOne(item)
-        resolve(addedItem.ops[0])
-        client.close()
+        const addedItem = await db.collection("newspapers").insertOne(item);
+        resolve(addedItem.ops[0]);
+        client.close();
       } catch (error) {
-        reject(error)
+        reject(error);
       }
-    })
+    });
   }
 
-  function update(id, newItem){
-    return new Promise(async(resolve, reject) => {
+  function update(id, newItem) {
+    return new Promise(async (resolve, reject) => {
       const client = new MongoClient(url);
       try {
         await client.connect();
         const db = client.db(dbName);
-        const updatedItem = await db.collection('newspapers')
-          .findOneAndReplace({_id: ObjectID(id)}, newItem, {returnOriginal:false});
+        const updatedItem = await db
+          .collection("newspapers")
+          .findOneAndReplace({ _id: ObjectID(id) }, newItem, {
+            returnOriginal: false,
+          });
 
-        resolve(updatedItem.value)
-        client.close()
+        resolve(updatedItem.value);
+        client.close();
       } catch (error) {
-        reject(error)
+        reject(error);
       }
-    })
+    });
   }
-  
-  function remove(id){
-    return new Promise(async(resolve, reject) => {
+
+  function remove(id) {
+    return new Promise(async (resolve, reject) => {
       const client = new MongoClient(url);
       try {
         await client.connect();
         const db = client.db(dbName);
-        const removed = await db.collection('newspapers').deleteOne({_id: ObjectID(id)});
-
-        resolve(removed.deleteCount === 1);
-        client.close()
+        const removed = await db.collection("newspapers").deleteOne({
+          _id: ObjectID(id),
+        });
+        resolve(removed.deletedCount === 1);
+        client.close();
       } catch (error) {
-        reject(error)
+        reject(error);
       }
-    })
+    });
   }
 
-  return {loadData, get, getById, add, update, remove}
+  function averageFinalists() {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(url);
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+        const average = await db
+          .collection("newspapers")
+          .aggregate([
+            {
+              $group: {
+                _id: null,
+                avgFinalists: {
+                  $avg: "$Pulitzer Prize Winners and Finalists, 1990-2014",
+                },
+              },
+            },
+          ])
+          .toArray();
+        resolve(average[0].avgFinalists);
+        client.close();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  function averageFinalistsByChange() {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(url);
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+        const average = await db
+          .collection("newspapers")
+          .aggregate([
+            {
+              $project: {
+                Newspaper: 1,
+                "Pulitzer Prize Winners and Finalists, 1990-2014": 1,
+                "Change in Daily Circulation, 2004-2013": 1,
+                overallChange: {
+                  $cond: {
+                    if: {
+                      $gte: ["$Change in Daily Circulation, 2004-2013", 0],
+                    },
+                    then: "positive",
+                    else: "negative",
+                  },
+                },
+              },
+            },
+            {
+              $group: {
+                _id: "$overallChange",
+                avgFinalists: {
+                  $avg: "$Pulitzer Prize Winners and Finalists, 1990-2014",
+                },
+              },
+            },
+          ])
+          .toArray();
+        resolve(average);
+        client.close();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+  return {
+    loadData,
+    get,
+    getById,
+    add,
+    update,
+    remove,
+    averageFinalists,
+    averageFinalistsByChange,
+  };
 }
 
 module.exports = circulationRepo();
